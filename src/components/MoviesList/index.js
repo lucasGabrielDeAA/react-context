e68e2cc6e8b2f1f9fd4e090cbb4400b271b568ba
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-11 14:46:51 -0300

    Visual adjustments on MoviesList's component

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -9,9 +9,19 @@ import { Movie } from './components';
 function MoviesList() {
   const [movies, setMovies] = useContext(MovieContext);
 
+  function handleAddMovie() {
+    const movie = { id: Math.random(), title: 'Batman - The dark knight', price: 50 };
+  }
+
   return (
     <div className="container">
-      {movies.map(movie => <Movie data={movie} />)}
+      <p className="title">Movies</p>
+
+      <div className="content">
+        {movies.map(movie => (
+          <Movie key={movie.id} data={movie} />
+        ))}
+      </div>
     </div>
   );
 }
