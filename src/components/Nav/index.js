e6c56d7dc87af0bbb1e097aa74434195efb250c2
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-11 14:52:42 -0300

    Nav's component rewrited

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,18 @@ import React, { useContext } from 'react';
 
 import { MovieContext } from '../../MovieContext';
 
-import './styles.css';
+import { Container, Content, Item } from './styles';
 
 function Nav() {
   const [movies, setMovies] = useContext(MovieContext);
+
   return (
-    <nav className="nav">
-      <div>
-        <p>Technus's movies list</p>
-        <p>{`Total (${movies.length})`}</p>
-      </div>
-    </nav>
+    <Container>
+      <Content>
+        <Item>Technus's movies list</Item>
+        <Item>{`Total (${movies.length})`}</Item>
+      </Content>
+    </Container>
   )
 }
 
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,13 +1,15 @@
-.nav {
+import styled from 'styled-components';
+
+export const Container = styled.nav`
   background: #babaca;
   box-shadow: 5px 10px 18px rgba(226, 226, 226, 0.7);
   left: 0;
   position: fixed;
   top: 0;
   width: 100%;
-}
+`;
 
-.nav div {
+export const Content = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
@@ -15,4 +17,6 @@
   justify-content: space-between;
   margin-left: 30px;
   margin-right: 30px;
-}
+`;
+
+export const Item = styled.p``;
