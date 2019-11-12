df729a98afc6a0c3e4bc3c623aaa9dc45c6ec5b6
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-12 10:40:44 -0300

    movies list persisted on localStorage

--- src/MovieContext.js
+++ src/MovieContext.js
@@ -1,16 +1,31 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 export const MovieContext = createContext();
 
 export const MovieProvider = ({ children }) => {
-  const [movies, setMovies] = useState([
-    { id: Math.random(), title: 'Lord of the rings - The fellowship of the ring', price: 20 },
-    { id: Math.random(), title: 'Avengers - Endgame', price: 35 },
-    { id: Math.random(), title: 'Star wars - Return of Jedi', price: 25 },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    loadMovies();
+  }, []);
+
+  async function loadMovies() {
+    const data = await localStorage.getItem('@movies:list');
+
+    if (data !== null) {
+      setMovies(JSON.parse(data));
+    }
+  }
+
+  async function handleMovies(movie) {
+    const data = [ ...movies, movie ];
+
+    localStorage.setItem('@movies:list', JSON.stringify(data));
+    setMovies(data);
+  }
 
   return (
-    <MovieContext.Provider value={[movies, setMovies]}>
+    <MovieContext.Provider value={[movies, handleMovies]}>
       {children}
     </MovieContext.Provider>
   )
